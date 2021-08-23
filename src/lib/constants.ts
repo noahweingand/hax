63245b14c4e645e11a4e430a27065a59b452f6ec
Noah Weingand <noahweingand@gmail.com>
2021-08-23 18:15:41 -0400

    changing commands

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -52,7 +52,22 @@ export const HaxPlayers = [
 export const commands = [
   new SlashCommandBuilder()
     .setName('stats')
-    .setDescription(`Get global stats for a HAX player`),
+    .setDescription(`Get global stats for a HAX player`)
+    .addStringOption((option: SlashCommandStringOption) =>
+      option
+        .setName('name')
+        .setDescription(`Name of the user you want stats for`)
+        .addChoices([
+          ['Austin', 'Austin'],
+          ['Ben', 'Ben'],
+          ['Brent', 'Brent'],
+          ['Malik', 'Malik'],
+          ['Pat', 'Pat'],
+          ['Tyler', 'Tyler'],
+          ['Zach', 'Zach'],
+        ])
+        .setRequired(false),
+    ),
   new SlashCommandBuilder()
     .setName('hero-perf')
     .setDescription(`Grabs hero specific performance by a HAX player`)
@@ -61,13 +76,58 @@ export const commands = [
         .setName('hero')
         .setDescription(`Dota hero's name`)
         .setRequired(true),
+    )
+    .addStringOption((option: SlashCommandStringOption) =>
+      option
+        .setName('name')
+        .setDescription(`Name of the user you want stats for`)
+        .addChoices([
+          ['Austin', 'austin'],
+          ['Ben', 'ben'],
+          ['Brent', 'brent'],
+          ['Malik', 'malik'],
+          ['Pat', 'pat'],
+          ['Tyler', 'tyler'],
+          ['Zach', 'zach'],
+        ])
+        .setRequired(false),
     ),
   new SlashCommandBuilder()
     .setName('last-match')
-    .setDescription(`Retrieves last match detail`),
+    .setDescription(`Retrieves last match detail`)
+    .addStringOption((option: SlashCommandStringOption) =>
+      option
+        .setName('name')
+        .setDescription(`Name of the user you want stats for`)
+        .addChoices([
+          ['Austin', 'austin'],
+          ['Ben', 'ben'],
+          ['Brent', 'brent'],
+          ['Malik', 'malik'],
+          ['Pat', 'pat'],
+          ['Tyler', 'tyler'],
+          ['Zach', 'zach'],
+        ])
+        .setRequired(false),
+    ),
   new SlashCommandBuilder()
     .setName('played-with')
-    .setDescription(`Returns matches played with either pros or casters`),
+    .setDescription(`Returns matches played with either pros or casters`)
+    .addStringOption((option: SlashCommandStringOption) =>
+      option
+        .setName('name')
+        .setDescription(`Name of the user you want stats for`)
+        .addChoices([
+          ['Austin', 'austin'],
+          ['Ben', 'ben'],
+          ['Brent', 'brent'],
+          ['Malik', 'malik'],
+          ['Pat', 'pat'],
+          ['Tyler', 'tyler'],
+          ['Zach', 'zach'],
+        ])
+        .setRequired(false),
+    ),
 ].map((command) => command.toJSON());
 
 declare global {
